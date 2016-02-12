4f1567c562d73f2a523538be4478531e5961c6f7
suiyujin <suiyujin04@gmail.com>
2016-02-13 08:01:43 +0900

    deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,17 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'manhattan-movie'
+set :repo_url, 'git@github.com:suiyujin/manhattan-movie.git'
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, ENV.fetch('DEPLOY_TO')
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -20,29 +20,47 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :log_level, :debug
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('.env', 'config/database.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
+set :rbenv_type, :user
+set :rbenv_ruby, '2.2.3'
+set :rbenv_path, '~/.anyenv/envs/rbenv'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w(rake gem bundle ruby rails)
+set :rbenv_roles, :all
+
+set :unicorn_pid, "#{shared_path}/tmp/pids/unicorn.pid"
+
+after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  task :start do
+    on roles(:all) do
+      execute "cd #{current_path} && (RAILS_ENV=#{fetch(:rails_env)} #{fetch(:rbenv_prefix)} bundle exec rake unicorn:start)"
     end
   end
 
+  task :stop do
+    on roles(:all) do
+      execute "cd #{current_path} && (RAILS_ENV=#{fetch(:rails_env)} #{fetch(:rbenv_prefix)} bundle exec rake unicorn:stop)"
+    end
+  end
+
+  task :restart do
+    on roles(:all) do
+      execute "cd #{current_path} && (RAILS_ENV=#{fetch(:rails_env)} #{fetch(:rbenv_prefix)} bundle exec rake unicorn:stop)"
+      execute "cd #{current_path} && (RAILS_ENV=#{fetch(:rails_env)} #{fetch(:rbenv_prefix)} bundle exec rake unicorn:start)"
+    end
+  end
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,8 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-
-
+server ENV.fetch('SERVER'), user: ENV.fetch('SSH_USER'), roles: %w(app db web)
 
 # role-based syntax
 # ==================
@@ -20,8 +19,9 @@
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
-
-
+role :app, ["#{ENV.fetch('SSH_USER')}@#{ENV.fetch('SERVER')}"]
+role :web, ["#{ENV.fetch('SSH_USER')}@#{ENV.fetch('SERVER')}"]
+role :db,  ["#{ENV.fetch('SSH_USER')}@#{ENV.fetch('SERVER')}"]
 
 # Configuration
 # =============
@@ -30,8 +30,6 @@
 # For available Capistrano configuration variables see the documentation page.
 # http://capistranorb.com/documentation/getting-started/configuration/
 # Feel free to add new variables to customise your setup.
-
-
 
 # Custom SSH Options
 # ==================
@@ -46,6 +44,12 @@
 #    forward_agent: false,
 #    auth_methods: %w(password)
 #  }
+set :ssh_options, {
+  keys: [ENV.fetch('SSH_KEY_NAME')],
+  forward_agent: false,
+  auth_methods: %w(publickey),
+  port: ENV.fetch('SSH_PORT')
+}
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
